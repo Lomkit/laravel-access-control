6611afcab9e0c0b114e6a56d2119bf974292de51
Gautier DELEGLISE <gautier.deleglise@gmail.com>
2025-04-09 22:57:42 +0200

    Update StubsTest.php

--- tests/Unit/StubsTest.php
+++ tests/Unit/StubsTest.php
@@ -9,12 +9,12 @@ class StubsTest extends TestCase
     public function test_stubs_correctly_registered(): void
     {
         app(Dispatcher::class)->dispatch($event = new \Illuminate\Foundation\Events\PublishingStubs([]));
-
+        
         $this->assertEquals(
             [
-                '/app/src/Console/stubs/control.stub'           => 'controller.stub',
-                '/app/src/Console/stubs/perimeter.plain.stub'   => 'perimeter.plain.stub',
-                '/app/src/Console/stubs/perimeter.overlay.stub' => 'perimeter.overlay.stub',
+                realpath(__DIR__.'/../../src/Console/stubs/control.stub')           => 'controller.stub',
+                realpath(__DIR__.'/../../src/Console/stubs/perimeter.plain.stub')   => 'perimeter.plain.stub',
+                realpath(__DIR__.'/../../src/Console/stubs/perimeter.overlay.stub') => 'perimeter.overlay.stub',
             ],
             $event->stubs
         );
